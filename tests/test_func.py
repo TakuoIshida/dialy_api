bd6b3864bed0f9fdbd7284b29e4f221d2e0a670d
takuo_ishida <archersparadox.100@gmail.com>
2021-06-05 10:01:50 +0900

    複数の値で同時にテストする場合を追加

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,5 +1,7 @@
 from utils.func import *
 
+import pytest
+
 
 def test_add_01():
     assert add(2.5, 3.5) == 6.0
@@ -11,3 +13,16 @@ def test_add_02():
 
 def test_add_03():
     assert add(0, 3.5) == 3.5
+
+# 複数のテストを同時に走らせる場合
+
+
+@pytest.mark.parametrize(
+    "x,y,expect", [
+        (2, 3, 5),
+        (1, 2, 3),
+        (4, 5, 9)
+    ]
+)
+def test_add_multi(x, y, expect: int):
+    assert x + y == expect
